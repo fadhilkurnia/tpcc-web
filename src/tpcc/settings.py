3305d307e4c58d15cc114b2d90ff60e7d921b86f
Fadhil Kurnia <fadhilimamk@gmail.com>
2024-05-15 17:44:35 -0400

    change default datastore to sqlite at /app/data/data.db

--- src/tpcc/settings.py
+++ src/tpcc/settings.py
@@ -1,4 +1,4 @@
 import os
 
-DATABASE_URL = os.environ.get("DATABASE_URL", "postgresql://localhost/bench_sa")
+DATABASE_URL = os.environ.get("DATABASE_URL", "sqlite:///data/data.db")
 WAREHOUSES = int(os.environ.get("WAREHOUSES", 10))
